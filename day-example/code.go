188e8b2fa3d353861bdd03218604430432448134
ThomasCDeveloper <33541076+ThomasCDeveloper@users.noreply.github.com>
2023-12-06 10:46:10 +0100

    2023-06

--- day-example/code.go
+++ day-example/code.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+// CMD: go run *.go
 
+import (
+	"fmt"
+)
+
+func SolvePart1(data []string) int {
+	return 0
+}
+
+func SolvePart2(data []string) int {
+	return 0
+}
+
+func main() {
+	data := GetInput("test.txt")
+
+	// PART 1
+	fmt.Println("Part 1:")
+	fmt.Println(SolvePart1(data))
+
+	// PART 2
+	fmt.Println("Part 2:")
+	fmt.Println(SolvePart2(data))
 }
