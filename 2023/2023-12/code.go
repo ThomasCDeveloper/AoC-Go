ae7d1c02e79147965800225773ff972e578ae131
ThomasCDeveloper <33541076+ThomasCDeveloper@users.noreply.github.com>
2023-12-12 19:52:20 +0100

    2023-12p2

--- 2023/2023-12/code.go
+++ 2023/2023-12/code.go
@@ -4,79 +4,99 @@ package main
 
 import (
 	"fmt"
-	"math"
 	"regexp"
 	"strconv"
 	"strings"
 )
 
-func isConform(cmd []int, springs string) bool {
-	for strings.Contains(springs, "..") {
-		springs = strings.Replace(springs, "..", ".", -1)
-	}
-	if string(springs[0]) == "." {
-		springs = springs[1:]
-	}
-	if len(springs) > 0 {
-		if string(springs[len(springs)-1]) == "." {
-			springs = springs[:len(springs)-1]
-		}
-	}
-
-	springChains := strings.Split(springs, ".")
-
-	if len(springChains) != len(cmd) {
-		return false
-	}
-
-	for i := range springChains {
-		if len(springChains[i]) != cmd[i] {
-			return false
-		}
-	}
-	return true
+type sequence struct {
+	springs string
+	cmd     string
 }
 
-func GetCombinaisons(cmd []int, springs string) int {
-	splitedSprings := strings.Split(springs, "?")
-	numberOfQuestionMarks := len(splitedSprings) - 1
+var cache = make(map[sequence]int)
+
+func GetCombinaisons(cmd []uint8, springs string) int {
+	if len(cmd) == 0 && len(springs) == 0 {
+		return 1
+	}
+	if len(springs) == 0 {
+		return 0
+	}
+
+	value, test := cache[sequence{springs, string(cmd)}]
+	if test { // si la sequence est déjà dans le cache
+		return value
+	}
+
+	if springs[0] == '.' {
+		val := GetCombinaisons(cmd, springs[1:])
+		cache[sequence{springs, string(cmd)}] = val
+		return val
+	}
 
 	sum := 0
-	for i := 0; i < int(math.Pow(2, float64(numberOfQuestionMarks))); i++ {
-		newSprings := ""
-		replaceQuestionMarks := strconv.FormatInt(int64(i), 2)
-		for len(replaceQuestionMarks) < numberOfQuestionMarks {
-			replaceQuestionMarks = "0" + replaceQuestionMarks
+	for _, n := range cmd {
+		sum += int(n)
+	}
+	if len(springs) < sum+len(cmd)-1 {
+		cache[sequence{springs, string(cmd)}] = 0
+		return 0
+	}
+
+	if springs[0] == '?' {
+		res := GetCombinaisons(cmd, springs[1:]) + GetCombinaisons(cmd, "#"+springs[1:])
+		cache[sequence{springs, string(cmd)}] = res
+		return res
+	}
+
+	if springs[0] == '#' {
+		if len(cmd) == 0 {
+			cache[sequence{springs, string(cmd)}] = 0
+			return 0
 		}
 
-		for j := 0; j < len(replaceQuestionMarks); j++ {
-			newSprings += splitedSprings[j]
-			if string(replaceQuestionMarks[j]) == "1" {
-				newSprings += "#"
-			} else {
-				newSprings += "."
-			}
+		n := cmd[0]
+		indexDot := strings.Index(springs, ".")
+		if indexDot == -1 {
+			indexDot = len(springs)
 		}
-		newSprings += splitedSprings[numberOfQuestionMarks]
+		if indexDot < int(n) {
+			cache[sequence{springs, string(cmd)}] = 0
+			return 0
+		}
 
-		if isConform(cmd, newSprings) {
-			sum += 1
+		remaining := springs[n:]
+		if len(remaining) == 0 {
+			res := GetCombinaisons(cmd[1:], remaining)
+			cache[sequence{springs, string(cmd)}] = res
+			return res
 		}
+
+		if remaining[0] == '#' {
+			cache[sequence{springs, string(cmd)}] = 0
+			return 0
+		}
+
+		res := GetCombinaisons(cmd[1:], remaining[1:])
+		cache[sequence{springs, string(cmd)}] = res
+		return res
 	}
 
 	return sum
 }
 
 func SolvePart1(data []string) int {
-	cmds := [][]int{}
+	cmds := [][]uint8{}
 	springss := []string{}
 	for _, line := range data {
 		re := regexp.MustCompile("[0-9]+")
 
-		cmd := []int{}
+		cmd := []uint8{}
 		for _, stringcmd := range re.FindAllString(line, -1) {
 			intcmd, _ := strconv.Atoi(stringcmd)
-			cmd = append(cmd, intcmd)
+			int8cmd := uint8(intcmd)
+			cmd = append(cmd, int8cmd)
 		}
 
 		cmds = append(cmds, cmd)
@@ -92,11 +112,38 @@ func SolvePart1(data []string) int {
 }
 
 func SolvePart2(data []string) int {
-	return 0
+	cmds := [][]uint8{}
+	springss := []string{}
+	for _, line := range data {
+		re := regexp.MustCompile("[0-9]+")
+
+		cmd := []uint8{}
+		for _, stringcmd := range re.FindAllString(line, -1) {
+			intcmd, _ := strconv.Atoi(stringcmd)
+			int8cmd := uint8(intcmd)
+			cmd = append(cmd, int8cmd)
+		}
+
+		cmds = append(cmds, cmd)
+		springss = append(springss, strings.Split(line, " ")[0])
+	}
+
+	sum := 0
+	for i := range springss {
+		cmd := []uint8{}
+		springs := ""
+		for j := 0; j < 5; j++ {
+			cmd = append(cmd, cmds[i]...)
+			springs += "?" + springss[i]
+		}
+		sum += GetCombinaisons(cmd, springs[1:])
+	}
+
+	return sum
 }
 
 func main() {
-	data := GetInput("input.txt")
+	data := GetInput("test.txt")
 
 	// PART 1
 	fmt.Println("Part 1:", SolvePart1(data))
