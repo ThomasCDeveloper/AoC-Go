b35f21b0f30a197a6a035557488400c3d6b679fa
ThomasCDeveloper <33541076+ThomasCDeveloper@users.noreply.github.com>
2023-12-24 09:43:13 +0100

    2023-22

--- 2023/2023-21/code.go
+++ 2023/2023-21/code.go
@@ -12,6 +12,7 @@ type co struct {
 }
 
 var tiles = map[co]bool{}
+var distances = map[co]int{}
 
 func printSituation(list []co) {
 	fmt.Println()
@@ -71,8 +72,9 @@ func SolvePart1(data []string) int {
 	}
 
 	visitedTiles := []co{startCo}
+	distances[startCo] = 0
 
-	for i := 0; i < 6; i++ {
+	for i := 0; i < 131; i++ {
 		newVisitedTiles := []co{}
 		for _, visitedTile := range visitedTiles {
 			for _, dir := range []co{{-1, 0}, {1, 0}, {0, 1}, {0, -1}} {
@@ -81,73 +83,47 @@ func SolvePart1(data []string) int {
 					if !slices.Contains(newVisitedTiles, rc) {
 						newVisitedTiles = append(newVisitedTiles, rc)
 					}
+					if val, check := distances[rc]; val == 0 || !check {
+						distances[rc] = i + 1
+					}
 				}
 			}
 		}
 		visitedTiles = newVisitedTiles
 	}
 
-	return len(visitedTiles)
-}
-
-func SolvePart2(data []string) int {
-	startCo := co{0, 0}
-	for r, line := range data {
-		for c, char := range line {
-			rcco := co{r, c}
-			if char == 'S' {
-				startCo = co{r, c}
-			}
-			if char == '#' {
-				tiles[rcco] = false
-			} else {
-				tiles[rcco] = true
-			}
-		}
-	}
-
-	visitedTilesBefore := []co{}
-	visitedTiles := []co{startCo}
-	newVisitedTiles := []co{}
 	sum := 0
 
-	nbPas := 0
-
-	for i := 0; i < 65+131; i++ {
-		nbPas++
-
-		newVisitedTiles = []co{}
-
-		for _, visitedTile := range visitedTiles {
-			for _, dir := range []co{{-1, 0}, {1, 0}, {0, 1}, {0, -1}} {
-				rc := co{visitedTile.r + dir.r, visitedTile.c + dir.c}
-				if getTile(rc, true) {
-					if !slices.Contains(visitedTilesBefore, rc) {
-						if !slices.Contains(newVisitedTiles, rc) {
-							newVisitedTiles = append(newVisitedTiles, rc)
-						}
-					}
-				}
-			}
+	for key, val := range distances {
+		if (key.c+key.r)%2 == 0 && val <= 65 {
+			sum += 1
 		}
-
-		if i%2 == 1 {
-			sum += len(newVisitedTiles)
-		}
-		if i%131 == 0 {
-			fmt.Println(i, sum)
-		}
-
-		visitedTilesBefore = deepCopy(visitedTiles)
-		visitedTiles = deepCopy(newVisitedTiles)
-	}
-
-	for i := 0; i < 202299; i++ {
-		nbPas += 131
-		sum += 948 + (i+1)*472
 	}
 
 	return sum
+}
+
+func SolvePart2(data []string) int {
+	evenCorners, oddCorners, evenFull, oddFull := 0, 0, 0, 0
+
+	for key, val := range distances {
+		if val > 65 {
+			if (key.c+key.r)%2 == 0 {
+				evenCorners++
+			} else {
+				oddCorners++
+			}
+		}
+		if (key.c+key.r)%2 == 0 {
+			evenFull++
+		} else {
+			oddFull++
+		}
+	}
+
+	n := 202300
+
+	return ((n+1)*(n+1)*oddFull + n*n*evenFull - (n+1)*oddCorners + n*evenCorners)
 }
 
 var data = []string{}
